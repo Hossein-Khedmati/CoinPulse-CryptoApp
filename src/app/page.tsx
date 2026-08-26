791d36df2a2fc19f2a6b47a08e1dee77d3128f28
Hossein Khedmati <khedmati.hossein1382@gmail.com>
2026-08-26 13:19:26 +0330

    implement coins overview and trending conis ui and functionality using the fetcher utility that w write and connect to coingecko

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-const Page = () => {
-  return (
-    <div></div>
-  )
-}
+import DataTable from "@/components/DataTable";
+import CoinOverview, {
+  CoinOverviewFallback,
+} from "@/components/home/CoinOverview";
+import TrendingCoins, {
+  TrendingCoinsFallback,
+} from "@/components/home/TrendingCoins";
+import { Suspense } from "react";
 
-export default Page
+const Page = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
